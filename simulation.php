2d74e269b16f6af2eafaae20e904293022bd15df
Elmer Rodriguez <elmer.rodriguez6@gmail.com>
2016-11-22 00:25:48 -0500

    Some documentation, comments were added to main files in the directory
    Changes were made in the index.php, roleplaying.php, action.php and
    colophon.php files. This include some information about the team
    (commented it out) and a brief description of the assignment in order
    to provider some context.

--- simulation.php
+++ simulation.php
@@ -1,126 +1,141 @@
+<!--
+Name: TEAM02-Team Adonding Poncho
+Project 2: Improving Customer Website
+Institution: University of Rochester
+Professor Kostin
+Semester: Fall 2016
+CSC174: Advanced Front End Deveploment
+Team Members: Evan Basta, Joshua Anes, Ethan Pupkin, Rayan Ifthikhar
+All Rights Reserved
+-->
+
+<!--Description-->
+
+<!--The purpose of this assignment if for Team02-Team Adonding Poncho to make changes to the customer website as stated on our proposal. The customer on this team is Team10-One Man Army. The orginal source for this project can be found in the following repository: https://github.com/J-Zhang104/CSC174-Group-10 -->
+
 <?php include "inc/head.inc"; ?>
 
 
 <!--Navigation -->
-				
+
  <?php include "inc/nav.inc"; ?>
 
   <body class="parallax">
 
-		
+
     <!-- this class will put article and aside side by side -->
     <div class="o-grid o-grid--small-full o-grid--medium-full o-grid--large-full">
 
-<!--Article -->				
+<!--Article -->
                 <article class="o-grid__cell">
 
-				
+
 	<!-- From: https://en.wikipedia.org/wiki/Simulation_video_game -->
 						 <h2 class="c-heading c-heading--xlarge">Simulation video game</h2>
-				
-							 <p>A simulation video game describes a diverse super-category of video games, generally designed to closely simulate aspects of a real or fictional reality.</p>	
-							 
+
+							 <p>A simulation video game describes a diverse super-category of video games, generally designed to closely simulate aspects of a real or fictional reality.</p>
+
 
 <div class="c-card c-card--higher c-card--primary">
  <div class="c-card__content c-card__content--divider">SimCity. <a class="c-link" href="http://www.whitedoveofhope.org/city-simulation-like-no-other-with-simcity-buildit/"> Source</a></div>
-							 <figure>					 
+							 <figure>
 						          <img class="o-image" src="images/simCity.jpg" alt="SimCity screenshot">
-							     
-							 
+
+
 						     </figure>
 
 						 </div>
-							 
-							 <p>A simulation game attempts to copy various activities from real life in the form of a game for various purposes such as training, analysis, or prediction. Usually there are no strictly defined goals in the game, with players instead allowed to freely control a character. Well-known examples are war games, business games, and role play simulation.</p>	
+
+							 <p>A simulation game attempts to copy various activities from real life in the form of a game for various purposes such as training, analysis, or prediction. Usually there are no strictly defined goals in the game, with players instead allowed to freely control a character. Well-known examples are war games, business games, and role play simulation.</p>
 
 
 <div class="c-card c-card--higher c-card--primary">
  <div class="c-card__content c-card__content--divider">Airport41. <a class="c-link" href="http://acknowledgetransmission.com/wp-content/uploads/2015/09/Airport41.jpg"> Source</a></div>
-							 <figure>					 
+							 <figure>
 						          <img class="o-image" src="images/airport.jpg" alt="Airport screenshot">
-							 
+
 						     </figure>
 							 </div>
-							 
-							 <p>From three basic types of strategic, planning, and learning exercises: games, simulations, and case studies, a number of hybrids may be considered, including simulation games that are used as case studies.</p>	
-							 
-						 <h3 class="c-heading c-heading--large">History</h3>
-						
-							 <p>While many credit simulation games beginning with Will Wright and SimCity in 1989, the true progenitor of the genre was "Fortune Builder", released in 1984 on Colecovision. Certain games such as SimLife and SimEarth were subsequently created and are capable of teaching players the basics of genetics and global ecosystems.</p>	
 
-							 <p>In a study where adolescents played SimCity 2000, the study found that those participants who played the game had a greater appreciation and expectation of their government officials after playing.</p>	
-							 
-						 <h3 class="c-heading c-heading--large">Construction and management simulation</h3>		 
-							 
-							 <p>Construction and management simulation (CMS) is a type of simulation game in which players build, expand or manage fictional communities or projects with limited resources. Strategy games sometimes incorporate CMS aspects into their game economy, as players must manage resources while expanding their project. But pure CMS games differ from strategy games in that "the player's goal is not to defeat an enemy, but to build something within the context of an ongoing process." Games in this category are sometimes also called "management games".</p>	
-							 
-							 
-						 <h3 class="c-heading c-heading--large">Life simulation</h3>		 
-							 
-							 <p>Life simulation games (or artificial life games) is a subgenre of simulation video games in which the player lives or controls one or more artificial lifeforms. A life simulation game can revolve around "individuals and relationships, or it could be a simulation of an ecosystem".</p>	
-							 
-							 
-						 <h3 class="c-heading c-heading--large">Sports</h3>		 
-							 
-							 <p>A sports game is a video game that simulates the playing of sports. Most sports have been recreated with a game, including team sports, athletics and extreme sports. Some games emphasize actually playing the sport (such as the Madden NFL series), whilst others emphasize strategy and organization (such as Championship Manager). Some, such as Arch Rivals, satirize the sport for comic effect. This genre has been popular throughout the history of video games and is competitive, just like real-world sports. A number of game series feature the names and characteristics of real teams and players, and are updated annually to reflect real-world changes.</p>	
-							 							 
-	
-	
+							 <p>From three basic types of strategic, planning, and learning exercises: games, simulations, and case studies, a number of hybrids may be considered, including simulation games that are used as case studies.</p>
+
+						 <h3 class="c-heading c-heading--large">History</h3>
+
+							 <p>While many credit simulation games beginning with Will Wright and SimCity in 1989, the true progenitor of the genre was "Fortune Builder", released in 1984 on Colecovision. Certain games such as SimLife and SimEarth were subsequently created and are capable of teaching players the basics of genetics and global ecosystems.</p>
+
+							 <p>In a study where adolescents played SimCity 2000, the study found that those participants who played the game had a greater appreciation and expectation of their government officials after playing.</p>
+
+						 <h3 class="c-heading c-heading--large">Construction and management simulation</h3>
+
+							 <p>Construction and management simulation (CMS) is a type of simulation game in which players build, expand or manage fictional communities or projects with limited resources. Strategy games sometimes incorporate CMS aspects into their game economy, as players must manage resources while expanding their project. But pure CMS games differ from strategy games in that "the player's goal is not to defeat an enemy, but to build something within the context of an ongoing process." Games in this category are sometimes also called "management games".</p>
+
+
+						 <h3 class="c-heading c-heading--large">Life simulation</h3>
+
+							 <p>Life simulation games (or artificial life games) is a subgenre of simulation video games in which the player lives or controls one or more artificial lifeforms. A life simulation game can revolve around "individuals and relationships, or it could be a simulation of an ecosystem".</p>
+
+
+						 <h3 class="c-heading c-heading--large">Sports</h3>
+
+							 <p>A sports game is a video game that simulates the playing of sports. Most sports have been recreated with a game, including team sports, athletics and extreme sports. Some games emphasize actually playing the sport (such as the Madden NFL series), whilst others emphasize strategy and organization (such as Championship Manager). Some, such as Arch Rivals, satirize the sport for comic effect. This genre has been popular throughout the history of video games and is competitive, just like real-world sports. A number of game series feature the names and characteristics of real teams and players, and are updated annually to reflect real-world changes.</p>
+
+
+
 			    </article>
-				        
+
 <!-- Aside -->
                 <aside class="o-grid__cell">
-				
+
 							<!--From: https://en.wikipedia.org/wiki/Take_on_Helicopters -->
-				             <p><em>Take on Helicopters</em></p> 
-					   
-							 <p>Take On Helicopters (TKOH) is a 2011 video game developed by Bohemia Interactive. It was released 27 October 2011 for Microsoft Windows, with its first official DLC, Take On Helicopters: Hinds being released 15 March 2012. The DLC features three different versions of the Mi-24 Hind (Mil Mi-24 V, Mi-24 P and SuperHind Mk.III). It is the first game in the Take On video game series, followed by Take on Mars.</p>	
+				             <p><em>Take on Helicopters</em></p>
+
+							 <p>Take On Helicopters (TKOH) is a 2011 video game developed by Bohemia Interactive. It was released 27 October 2011 for Microsoft Windows, with its first official DLC, Take On Helicopters: Hinds being released 15 March 2012. The DLC features three different versions of the Mi-24 Hind (Mil Mi-24 V, Mi-24 P and SuperHind Mk.III). It is the first game in the Take On video game series, followed by Take on Mars.</p>
 
 
 <div class="c-card c-card--higher c-card--primary">
  <div class="c-card__content c-card__content--divider">Take on Helicopters. <a class="c-link" href="http://acknowledgetransmission.com/screenshot/take-on-helicopters-screenshots/"> Source</a></div>
-							 <figure>					 
+							 <figure>
 						          <img class="o-image" src="images/helicopter2.jpg" alt="screen shot">
-						     </figure>							 
+						     </figure>
 							 </div>
 
-							 <p>The campaign takes place primarily in the then-future of 2013, on a 3,800 square kilometer section of Seattle, Washington and its surrounding metropolitan area. In the campaign mode, the player takes on the role of a civilian helicopter pilot, Tom Larkin, whose civil aviation business is struggling through hard times. The campaign involves different types of contracts which the player earns money, which can be used to buy, repair, and upgrade helicopters. The campaign also features flashback missions, where the player takes on the role of Larkin's older brother, Joe Larkin, during his military service as a U.S. Army helicopter pilot during a 2012 war in Takistan, the fictional country that was the setting of Bohemia Interactive's 2010 video game, ARMA 2: Operation Arrowhead.</p>	
+							 <p>The campaign takes place primarily in the then-future of 2013, on a 3,800 square kilometer section of Seattle, Washington and its surrounding metropolitan area. In the campaign mode, the player takes on the role of a civilian helicopter pilot, Tom Larkin, whose civil aviation business is struggling through hard times. The campaign involves different types of contracts which the player earns money, which can be used to buy, repair, and upgrade helicopters. The campaign also features flashback missions, where the player takes on the role of Larkin's older brother, Joe Larkin, during his military service as a U.S. Army helicopter pilot during a 2012 war in Takistan, the fictional country that was the setting of Bohemia Interactive's 2010 video game, ARMA 2: Operation Arrowhead.</p>
 
-							 <p>In the summer of 2013, former U.S. Army helicopter pilot and war veteran, Joe Larkin, and his younger brother, Tom Larkin, take over their Seattle-based family business, civil aviation provider, Larkin Aviation, after the death of their father, who was also the owner. Only mere weeks away from closure, the Larkin brothers face an uncertain future in a struggling economy. Trying to save what their father has built, they decide to take on contracts, and competitors.</p>	
+							 <p>In the summer of 2013, former U.S. Army helicopter pilot and war veteran, Joe Larkin, and his younger brother, Tom Larkin, take over their Seattle-based family business, civil aviation provider, Larkin Aviation, after the death of their father, who was also the owner. Only mere weeks away from closure, the Larkin brothers face an uncertain future in a struggling economy. Trying to save what their father has built, they decide to take on contracts, and competitors.</p>
 
-							 <p>Take On Helicopters is primarily a civil aviation video game focused on rotary-wing aircraft, featuring three different classes of helicopters: light, medium, and heavy. In addition to the career, the game includes challenges, time trials, training, a mission editor, free flight, and multiplayer.</p>	
-							 
-							 
+							 <p>Take On Helicopters is primarily a civil aviation video game focused on rotary-wing aircraft, featuring three different classes of helicopters: light, medium, and heavy. In addition to the career, the game includes challenges, time trials, training, a mission editor, free flight, and multiplayer.</p>
+
+
 							 <p><em>SimCity</em></p>
-							 
+
 							 <p>SimCity is a city-building and urban planning simulation massively multiplayer online game developed by Maxis, a subsidiary of Electronic Arts. Released for Microsoft Windows in early March 2013, it is the first major installment in the SimCity series since the release of SimCity 4 a decade prior. An OS X version was released on August 29, 2013.</p>
-							 
+
 							 <p>The game is considered to be a reboot of the SimCity series. Players can create a settlement that can grow into a city by zoning land for residential, commercial, or industrial development, as well as building and maintaining public services, transport and utilities. SimCity uses a new engine called GlassBox, allowing for more detailed simulation than previous games. </p>
-							 
+
 							 <p>Along with many of the cosmetic changes (such as up-to-date 3D graphics), SimCity uses the new GlassBox engine. "We try to build what you would expect to see, and that's the game," explains system architect Andrew Willmott, meaning that visual effects such as traffic, economic troubles, and pollution will be more obvious. Two other new features are a multiplayer component and finite resources.</p>
-							 
+
 							 <p>Cities in a region are connected to each other via predefined regional networks such as highways, railways, and waterways. Elements such as traffic and air pollution are visible flowing between cities. Cities can trade resources or share public services with their neighbors like garbage collection or health care. Cities can also pool their collective wealth and resources to build a "great work" to provide benefits for the entire region like a massive solar power plant or an international airport. The larger the region, the higher is the number of cities and great works that can be built.</p>
-							 
+
 							 <p>Modules in SimCity are attachable structures that can add functionality to existing user-placeable buildings. One example is the extra garage for fire stations, which can provide additional fire trucks for increased protection coverage Another example is the Department of Safety for the City Hall, which unlocks more advanced medical, police and fire department buildings.</P>
-							 
+
 							 <p>The user interface, which was inspired by Google Maps and infographics, was designed to convey information to the player more clearly than in previous SimCity games. Animations and color-coded visual cues that represent how efficiently a city functions are only presented when needed at any given moment. For instance, opening up the water tower instantly changes the landscape to a clear world where the density of water is recognizable. Or clicking on the sewage tab will immediately show how the waste of the citizens is flowing, and where the system is over capacity. Some of the other visualized data include air pollution, power distribution, police coverage, and zones.</p>
                 </aside>
 
             </div> <!--blaze css grid-->
 
-<!--Topical footer -->				
+<!--Topical footer -->
                 <footer>
-				
+
                         <h2 class="c-heading c-heading--xlarge">Works Cited</h2>
-	
-	
+
+
 						<ul class="c-list">
 							<li class="c-list__item">"Simulation video game". Wikipedia. 24 April 2016. Web. 26 September 2016. <a  class="c-link" href="https://en.wikipedia.org/wiki/Simulation_video_game">https://en.wikipedia.org/wiki/Simulation_video_game</a>.</li>
 							<li class="c-list__item">"Take On Helicopters". Wikipedia. 1 September 2016. Web. 26 September 2016. <a  class="c-link" href="https://en.wikipedia.org/wiki/Take_on_Helicopters">https://en.wikipedia.org/wiki/Take_on_Helicopters </a>.</li>
 						</ul>
-						
-						
+
+
                 </footer>
-				
+
 
  <?php include "inc/footer.inc"; ?>
