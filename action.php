e04f02d8ca87553231628c4a0c350b4d023f8361
Elmer Rodriguez <elmer.rodriguez6@gmail.com>
2016-12-23 16:20:04 -0500

    Revert "attempted to increase margins between sections"
    This reverts commit 1664371c648c11b785c3ef9baee13ef5b5a543ed.
    
    # Conflicts:
    #	css/blaze.min.css
    #	css/styles.css

--- action.php
+++ action.php
@@ -20,14 +20,14 @@ All Rights Reserved
 
   <body class="parallax">
 
-<div class="o-container o-container--large">			
+<div class="o-container o-container--large">
                 <div class="o-grid o-grid--wrap o-grid--demo">
 				<div class= "o-grid__cell o-grid__cell--width-70 o-grid__cell--width-33@small">
 	<!-- From: https://en.wikipedia.org/wiki/Action_game -->
 						 <h2 class="c-heading c-heading--xlarge">Action Video Games</h2>
-				
-							 <p>The action game is a video game genre that emphasizes physical challenges, including hand–eye coordination and reaction-time. The genre includes diverse subgenres such as fighting games, shooter games and platform games which are widely considered the most important action games, though some real-time strategy games are also considered to be action games.</p>	
-							 <p>In an action game, the player typically controls the avatar of a protagonist. The avatar must navigate a level, collecting objects, avoiding obstacles, and battling enemies with various attacks. At the end of a level or group of levels, the player must often defeat a large boss enemy that is larger and more challenging than other enemies. Enemy attacks and obstacles deplete the avatar's health and lives, and the game is over when the player runs out of lives. Alternatively, the player wins the game by finishing a sequence of levels. But some action games, usually arcade games, are unbeatable and have an indefinite number of levels; and the player's only goal is to maximize their score by collecting objects and defeating enemies.</p>	
+
+							 <p>The action game is a video game genre that emphasizes physical challenges, including hand–eye coordination and reaction-time. The genre includes diverse subgenres such as fighting games, shooter games and platform games which are widely considered the most important action games, though some real-time strategy games are also considered to be action games.</p>
+							 <p>In an action game, the player typically controls the avatar of a protagonist. The avatar must navigate a level, collecting objects, avoiding obstacles, and battling enemies with various attacks. At the end of a level or group of levels, the player must often defeat a large boss enemy that is larger and more challenging than other enemies. Enemy attacks and obstacles deplete the avatar's health and lives, and the game is over when the player runs out of lives. Alternatively, the player wins the game by finishing a sequence of levels. But some action games, usually arcade games, are unbeatable and have an indefinite number of levels; and the player's only goal is to maximize their score by collecting objects and defeating enemies.</p>
 							</div>
 							<div class="o-grid__cell o-grid__cell--width-30 o-grid__cell--width-33@small">
 
@@ -42,23 +42,23 @@ All Rights Reserved
 							 <div class="c-card c-card--higher c-card--primary">
 							 <div class="c-card__content c-card__content--divider">	An action video game character. <a class="c-link" href="http://picphotos.net/wallpapers-video-games/"> Source</a></div>
 
-							 	
+
 						 	</div>
 <!--<<<<<< HEAD-->
 						 </div>
 				</div>
-							 
-							
+
+
 							 <div class="o-grid o-grid--wrap o-grid--demo">
 							 	<div class= "o-grid__cell o-grid__cell--width-70 o-grid__cell--width-33@small">
 						<h2 class="c-heading c-heading--xlarge">Defining elements</h2>
-						
-							<p>The action genre includes any game where the majority of challenges are physical tests of skill. Action games can sometimes incorporate other challenges such as races, puzzles, or collecting objects, but they are not central to the genre. Players may also encounter tactical and exploration challenges, but these games first-and-foremost require high reaction speed and good hand–eye coordination. The player is often under time pressure, and there is not enough time for complex strategic planning. In general, faster action games are more challenging. Action games may sometimes involve puzzle solving, but they are usually quite simple because the player is under immense time pressure.</p>	
+
+							<p>The action genre includes any game where the majority of challenges are physical tests of skill. Action games can sometimes incorporate other challenges such as races, puzzles, or collecting objects, but they are not central to the genre. Players may also encounter tactical and exploration challenges, but these games first-and-foremost require high reaction speed and good hand–eye coordination. The player is often under time pressure, and there is not enough time for complex strategic planning. In general, faster action games are more challenging. Action games may sometimes involve puzzle solving, but they are usually quite simple because the player is under immense time pressure.</p>
 							<p>In an action game, the player typically controls the avatar of a protagonist. The avatar must navigate a level, collecting objects, avoiding obstacles, and battling enemies with various attacks. At the end of a level or group of levels, the player must often defeat a large boss enemy that is larger and more challenging than other enemies. Enemy attacks and obstacles deplete the avatar's health and lives, and the game is over when the player runs out of lives. Alternatively, the player wins the game by finishing a sequence of levels. But some action games, usually arcade games, are unbeatable and have an indefinite number of levels; and the player's only goal is to maximize their score by collecting objects and defeating enemies.</p>
 
 							</div>
-							</div>								 
-				
+							</div>
+
 							 <div class="o-grid o-grid--wrap o-grid--demo">
 							 <div class="o-grid__cell o-grid__cell--width-70 o-grid__cell--width-33@small">
 						 <h2 class="c-heading c-heading--xlarge">Game design</h2>
@@ -86,18 +86,18 @@ All Rights Reserved
 								<p>In many action games, the avatar has a certain amount of hitmarkers or health, which are depleted by enemy attacks and other hazards. Sometimes health can be replenished by collecting an in-game object. When the player runs out of health, the player dies. The player's avatar is often given a small number of chances to retry after death, typically referred to as lives. Upon beginning a new life, the player resumes the game either from the same location they died, a checkpoint, or the start of the level. Upon starting a new life, the avatar is typically invincible for a few seconds to allow the player to re-orient themselves. Players may earn extra lives by reaching a certain score or by finding an in-game object. Arcade games still limit the number of player lives, while home video games have shifted increasingly to unlimited lives.</p>
 
 
-							
-								</div>	
+
+								</div>
 
 
 
 								<div class="o-grid__cell o-grid__cell--width-30 o-grid__cell--width-33@small">
-									<figure>					 
-						          	<img class="csgo" src="images/csgo.jpg" alt="csgo hud"> 
+									<figure>
+						          	<img class="csgo" src="images/csgo.jpg" alt="csgo hud">
 						    	 </figure>
 						    	</div>
 						    </div>
-								<!-- end section 3-->						
+								<!-- end section 3-->
 			   <!-- </article>
 
 <!-- Aside -->
@@ -106,11 +106,11 @@ All Rights Reserved
 				<div class= "o-grid__cell o-grid__cell--width-33@small">
 							<!--From: https://en.wikipedia.org/wiki/Call_of_Duty_(video_game)  -->
 				             <h2 class="c-heading c-heading--xlarge">Call of Duty</h2>
-							 
+
 
 							  <p>As a first-person shooter, Call of Duty places the player in control of an infantry soldier who makes use of various authentic World War II firearms in combat. Each mission features a series of objectives which are marked on the heads-up display's compass; the player must complete all objectives to advance to the next mission. The player can save and load at any time, rather than the checkpoint system utilized in later Call of Duty games.</p>
-					   
-							 <p>The player has two primary weapon slots, a handgun slot and can carry up to ten grenades. Weapons may be exchanged with those found on the battlefield dropped by dead soldiers. Unlike later Call of Duty games, the first allows the player to toggle between different firing modes (single shot or automatic fire). Call of Duty was one of the early first-person shooters to feature iron sights in game play; by pressing the corresponding key the player aims down the actual sights on the gun for increased accuracy. In addition to weapons carried by the player, mounted machine guns and other fixed weapon emplacements are controllable by the player.</p>	
+
+							 <p>The player has two primary weapon slots, a handgun slot and can carry up to ten grenades. Weapons may be exchanged with those found on the battlefield dropped by dead soldiers. Unlike later Call of Duty games, the first allows the player to toggle between different firing modes (single shot or automatic fire). Call of Duty was one of the early first-person shooters to feature iron sights in game play; by pressing the corresponding key the player aims down the actual sights on the gun for increased accuracy. In addition to weapons carried by the player, mounted machine guns and other fixed weapon emplacements are controllable by the player.</p>
 
 				</div>
 			</div>
@@ -123,7 +123,7 @@ All Rights Reserved
 							<!--From: https://en.wikipedia.org/wiki/Call_of_Duty_(video_game)  -->
 				             <h2 class="c-heading c-heading--xlarge">Call of Duty</h2>
 
-							 
+
 
 							 <p>As a first-person shooter, Call of Duty places the player in control of an infantry soldier who makes use of various authentic World War II firearms in combat. Each mission features a series of objectives which are marked on the heads-up display's compass; the player must complete all objectives to advance to the next mission. The player can save and load at any time, rather than the checkpoint system utilized in later Call of Duty games.</p>
 
@@ -153,25 +153,25 @@ All Rights Reserved
 							  <div class="c-card c-card--higher c-card--primary">
 							 <div class="c-card__content c-card__content--divider">
 							 	Call of Duty Black Ops II <a class="c-link" href="http://getintopc.com/games/shooting-games/call-of-duty-black-ops-2-download-free-repack-single-link/"> Source</a></div>
-							 <figure>					 
+							 <figure>
 						          <img class="o-image" src="images/callOfDutyBlackOpsPic.jpg" alt="Call Of Duty Image">
 						     </figure>
-						     </div>	
+						     </div>
 						 </div>
-						</div> 
+						</div>
 
 
-							
+
 						<!-- end section 4-->
-	
 
-					
+
+
 <div class="o-grid o-grid--wrap o-grid--demo">
 	<div class="o-grid__cell o-grid__cell--width-70 o-grid__cell--width-33@small">
 
 	<h3 class="c-heading c-heading--large">Call of Duty: Black Ops III</h3>
 
-	 
+
 
 	<p>Black Ops III takes place in 2065, 40 years after the events of Black Ops II, in a world facing upheaval from climate change and new technologies. Similar to its predecessors, the story follows a group of black ops soldier. The game's campaign is designed to support 4-player cooperative gameplay, allowing for bigger, more open level design and less corridor shooting. As the player character is cybernetically enhanced, players have access to various special activities. The game also features a standalone Zombie mode, and a "Nightmare" mode which will be unlocked after the player completes all campaign missions.</p>
 
@@ -188,24 +188,24 @@ All Rights Reserved
 </div>
 <div class="o-grid o-grid--wrap o-grid--demo">
 	<div class="o-grid__cell o-grid__cell--width-33@small">
-<h4 class="c-heading c-heading--large">Campaign</h4>
+<h4 class="c-heading c-heading--xlarge">Campaign</h4>
 
 <p>The campaign in Black Ops III is designed to support 4-player cooperative gameplay, allowing for bigger, more open level design and less corridor shooting. In addition, the player can customize their character's gender and appearance. The campaign features its own progression system, featuring unlock tokens which must be used to acquire different weapons and gears as they progress through the campaign. The game features a "realistic" difficulty mode, in which players will get defeated if they are hit by one bullet. Finishing all campaign missions will also unlock "Nightmares" mode, where players can replay the entire campaign with a new narrative, as well as zombies replacing most of the normal enemies.</p>
 
-<h4 class="c-heading c-heading--large">Multiplayer</h4>
+<h4 class="c-heading c-heading--xlarge">Multiplayer</h4>
 
 <p>The multiplayer introduces a new momentum-based movement system, which utilizes thruster packs to allow players to perform slow boosts into the air, as well as perform wall running and sliding, all the while giving players complete gun control. In addition to the Pick 10 class system from Black Ops II, Treyarch implemented a character system called "Specialists", where players can pick from 9 different soldiers, each with either a special weapon or ability unique to them. A new "Gunsmith" feature offers aesthetic variations in weapon attachments, allowing various weapon customization combinations. The Paintshop feature allows players to create their own custom prints onto specific portions of a gun, further emphasizing the depth of customization in the game.</p>
 
-<h4 class="c-heading c-heading--large">Zombies</h4>
+<h4 class="c-heading c-heading--xlarge">Zombies</h4>
 
 <p>Zombies features a new XP progression system, which allows players to unlock items in a similar fashion to multiplayer and campaign. Unlockable items include "Gobblegums", which grant players with temporary bonuses, and weapon kits that allow players to modify the appearance of the guns in each game. Shadows of Evil was revealed as a new map for the Zombies mode on July 9, 2015 at San Diego Comic-Con International 2015. The map features four brand new characters: Nero the Magician, Jessica the Femme Fatale, Vincent the Cop and Campbell the Boxer as the main cast, and takes place in a new setting called Morg City. "The Giant" includes the original characters, Tank Dempsey, Edward Richtofen, Nikolai Belinski and Takeo Masaki. These characters return from the Black Ops II map "Origins", in their alternate timeline versions. They reappear in the DLC maps "The Giant", "Der Eisendrache", "Zetsubou no Shima", "Gorod Krovi" and "Revelations".</p>
 
-							 
- 
+
+
 </div>
 
  </div> <!--blaze css grid-->
-			
+
 
  </div> <!--blaze css grid-->
 </div><!--End: <div class="o-container o-container--large"> -->
